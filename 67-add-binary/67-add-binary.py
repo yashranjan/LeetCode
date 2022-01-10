01105481f1bdf99efed14c89ab20f818bcb59c0e
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-01-10 19:44:42 +0530

    Time: 54 ms (13.67%), Space: 14.3 MB (22.51%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,6 +1,6 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        carry, ans, i, j = 0, '', len(a)-1, len(b)-1
+        carry, ans, i, j = 0, [], len(a)-1, len(b)-1
         while i>=0 or j>=0 or carry:
             carry += int(a[i]) if i>=0 else 0
             carry += int(b[j]) if j>=0 else 0
@@ -8,5 +8,5 @@ class Solution:
             carry //= 2
             i, j = i-1, j-1
         ans = ans[::-1]
-        return ans
+        return ''.join(ans)
         
