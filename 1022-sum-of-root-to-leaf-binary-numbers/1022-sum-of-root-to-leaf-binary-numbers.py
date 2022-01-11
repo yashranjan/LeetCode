ffbe7ae9f8303bc20017d4874dbdc26c928c6e4b
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-01-11 20:34:17 +0530

    Time: 32 ms (92.41%), Space: 14.6 MB (39.83%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
@@ -6,11 +6,16 @@
 #         self.right = right
 
 class Solution:
-    def sumRootToLeaf(self, root: Optional[TreeNode], val=0) -> int:
-        if not root: return 0
-        val = val * 2 + root.val
-        if root.left == root.right: return val
-        return self.sumRootToLeaf(root.left, val) + self.sumRootToLeaf(root.right, val)
-            
-        
-        
+    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+        val = 0
+        stack = [(root, 0)]
+        while stack:
+            root, currVal = stack.pop()
+            if root:
+                currVal = (currVal<<1)|root.val
+                if not root.left and not root.right:
+                    val += currVal
+                else:
+                    stack.append((root.right, currVal))
+                    stack.append((root.left, currVal))
+        return val
