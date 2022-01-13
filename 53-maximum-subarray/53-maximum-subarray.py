659b6f092649fc82ccf9a246ffa43d459701b148
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-01-13 20:27:06 +0530

    Time: 4580 ms (5.02%), Space: 27.4 MB (99.69%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,17 +1,30 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        currSum, maxSum = 0, -math.inf
-        st, et = 0, 0
-        for idx, i in enumerate(nums):
-            if currSum == 0:
-                st = idx
-            currSum += i
-            if currSum > maxSum:
-                maxSum = currSum
-                et = idx
-            if currSum < 0:
-                currSum = 0
-        print(st, et)
-        return maxSum
+        def solve(nums, left, right):
+            if left > right:
+                return -math.inf
+            
+            mid = (left+right)//2
+
+            curr = 0
+            bestLeftSum = bestRightSum = 0
+
+            for i in range(mid-1, left-1, -1):
+                curr += nums[i]
+                bestLeftSum = max(bestLeftSum, curr)
+            
+            curr = 0
+            for i in range(mid+1, right+1):
+                curr += nums[i]
+                bestRightSum = max(bestRightSum, curr)
+            
+            bestCurrSum = bestLeftSum + bestRightSum + nums[mid]
+            
+            bestLeftSum = solve(nums, left, mid-1)
+            bestRightSum = solve(nums, mid+1, right)
+            
+            return max(bestCurrSum, bestLeftSum, bestRightSum)
+        
+        return solve(nums, 0, len(nums)-1)
         
         
