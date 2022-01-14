9145539aac829420ee5b5da2e8a7a6462f5fec77
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-01-14 12:06:43 +0530

    Time: 68 ms (6.31%), Space: 14.3 MB (57.28%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -1,24 +1,21 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        s = s.strip()
-        if not s:
-            return 0
-        isNegative = False
-        if s[0]=='-' or s[0]=='+':
-            if s[0] == '-': isNegative = True
-            s = s[1:]
-        t = []
-        for i in s:
-            if i.isdigit():
-                t.append(i)
-            else:
-                break
-        if not t:
-            return 0
-        val = int(''.join(t))
-        if isNegative:
-            val = -val
         intMin, intMax = -2**31, 2**31-1
-        val = max(intMin, val)
-        val = min(intMax, val)
-        return val
+        ind = ans = 0
+        negation = 1
+        n = len(s)
+        while ind < n and s[ind] == ' ':
+            ind += 1
+        if ind < n and s[ind] == '-':
+            negation = -1
+            ind += 1
+        elif ind < n and s[ind] == '+':
+            negation = 1
+            ind += 1
+        while ind < n and s[ind].isdigit():
+            currDigit = ord(s[ind])-ord('0')
+            if (ans > intMax//10) or (ans == intMax//10 and currDigit > intMax%10):
+                return intMax if negation == 1 else intMin
+            ans = (ans*10)+currDigit
+            ind += 1
+        return negation*ans
