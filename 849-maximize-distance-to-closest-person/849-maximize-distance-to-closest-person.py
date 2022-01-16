21759b9cce6ca5372a85c1cc0038e403d14b7f53
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-01-16 12:38:41 +0530

    Time: 196 ms (23.17%), Space: 14.8 MB (25.91%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.py
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.py
@@ -1,28 +1,22 @@
 class Solution:
     def maxDistToClosest(self, seats: List[int]) -> int:
         n = len(seats)
-        prefix = [-1]*n
-        suffix = [n]*n
-        for i in range(1, n):
-            if seats[i-1]:
-                prefix[i] = i-1
-            else:
-                prefix[i] = prefix[i-1]
-        for i in range(n-2, -1, -1):
-            if seats[i+1]:
-                suffix[i] = i+1
-            else:
-                suffix[i] = suffix[i+1]
+        prev, future = math.inf, 0
         ans = 0
         for i in range(n):
-            if seats[i] == 0:
-                minSeat = math.inf
-                if prefix[i] == -1:
-                    minSeat = suffix[i]-i
-                elif suffix[i] == n:
-                    minSeat = i-prefix[i]
-                else:
-                    minSeat = min((i-prefix[i], suffix[i]-i))
-                ans = max(ans, minSeat)
+            if seats[i]:
+                prev = i
+            else:
+                if future <= i:
+                    for j in range(i+1, n):
+                        if seats[j]:
+                            future = j
+                            break
+                    else:
+                        future = math.inf
+                leftSeat = i - prev if prev != math.inf else math.inf
+                rightSeat = future - i if future != math.inf else math.inf
+                ans = max(ans, min(leftSeat, rightSeat))
+        
         return ans
         
