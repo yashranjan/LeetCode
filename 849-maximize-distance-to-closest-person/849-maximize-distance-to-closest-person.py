76c69ca15d3cbe19eb51ec6035422b5698546b4b
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-01-16 12:08:08 +0530

    Time: 303 ms (5.06%), Space: 15.8 MB (5.54%) - LeetHub

---
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxDistToClosest(self, seats: List[int]) -> int:
+        n = len(seats)
+        prefix = [-1]*n
+        suffix = [n]*n
+        for i in range(1, n):
+            if seats[i-1]:
+                prefix[i] = i-1
+            else:
+                prefix[i] = prefix[i-1]
+        for i in range(n-2, -1, -1):
+            if seats[i+1]:
+                suffix[i] = i+1
+            else:
+                suffix[i] = suffix[i+1]
+        ans = 0
+        print(prefix, suffix)
+        for i in range(n):
+            if seats[i] == 0:
+                minSeat = math.inf
+                if prefix[i] == -1:
+                    minSeat = suffix[i]-i
+                elif suffix[i] == n:
+                    minSeat = i-prefix[i]
+                else:
+                    minSeat = min((i-prefix[i], suffix[i]-i))
+                ans = max(ans, minSeat)
+        return ans
+        
