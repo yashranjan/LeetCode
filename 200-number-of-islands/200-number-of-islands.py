17d9fa2b7c3a5f952d0fd0c9a964016c0eba9af5
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-01-16 14:31:57 +0530

    Time: 445 ms (27.15%), Space: 16.7 MB (69.90%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,22 +1,21 @@
 class Solution:
-    def dfs(self, grid, x, y, isVisited, n, m):
-        xyDir = [(-1,0), (0, -1), (0, 1), (1, 0)]
-        isVisited[x][y] = True
-        for i,j in xyDir:
-            currX, currY = x+i, y+j
-            if currX>=0 and currY>=0 and currX<n and currY<m and not isVisited[currX][currY] and grid[currX][currY]=='1':
-                self.dfs(grid, currX, currY, isVisited, n, m)
-        
-        
     def numIslands(self, grid: List[List[str]]) -> int:
         totIslands = 0
         n, m = len(grid), len(grid[0])
         isVisited = [[False]*m for i in range(n)]
+        xyDir = [(-1,0), (0, -1), (0, 1), (1, 0)]
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == '1' and not isVisited[i][j]:
                     totIslands += 1
-                    self.dfs(grid, i, j, isVisited, n, m)
+                    stack = [(i, j)]
+                    while stack:
+                        x, y = stack.pop()
+                        isVisited[x][y] = True
+                        for xDir, yDir in xyDir:
+                            currX, currY = x+xDir, y+yDir
+                            if currX>=0 and currY>=0 and currX<n and currY<m and not isVisited[currX][currY] and grid[currX][currY]=='1':
+                                stack.append((currX, currY))
 
         return totIslands
                     
