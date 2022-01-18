26f554da0199db4cc3f925cdea217e828c185f5e
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-01-18 20:44:36 +0530

    Time: 115 ms (26.26%), Space: 15.4 MB (14.11%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,4 +1,31 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        return heapq.nlargest(k, nums)[-1]
+        
+        def partition(left, right, pivot_index):
+            pivot = nums[pivot_index]
+            nums[pivot_index], nums[right] = nums[right], nums[pivot_index]
+            
+            store_index = left
+            for i in range(left, right):
+                if nums[i] < pivot:
+                    nums[store_index], nums[i] = nums[i], nums[store_index]
+                    store_index += 1
+            
+            nums[right], nums[store_index] = nums[store_index], nums[right]
+            
+            return store_index
+        
+        def quickselect(left, right, k_smallest):
+            if left == right:
+                return nums[left]
+            pivot_index = random.randint(left, right)
+            pivot_index = partition(left, right, pivot_index)
+            if k_smallest == pivot_index:
+                return nums[k_smallest]
+            elif k_smallest < pivot_index:
+                return quickselect(left, pivot_index-1, k_smallest)
+            else:
+                return quickselect(pivot_index+1, right, k_smallest)
+        
+        return quickselect(0, len(nums)-1, len(nums)-k)
         
