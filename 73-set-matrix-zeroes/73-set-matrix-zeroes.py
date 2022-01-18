67d047b8fe44d184e795272961c1c744e242e10b
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-01-18 21:35:57 +0530

    Time: 128 ms (82.58%), Space: 15 MB (91.39%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -4,14 +4,13 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n, m = len(matrix), len(matrix[0])
-        isFirstColZero = isFirstRowZero = False
+        isFirstColZero = False
         
         for i in range(n):
-            for j in range(m):
+            if matrix[i][0] == 0:
+                isFirstColZero = True
+            for j in range(1, m):
                 if matrix[i][j] == 0:
-                    if i==0 and j==0: isFirstColZero = isFirstRowZero = True
-                    elif i==0: isFirstRowZero = True
-                    elif j==0: isFirstColZero = True
                     matrix[i][0] = matrix[0][j] = 0
         
         for i in range(1, n):
@@ -19,7 +18,7 @@ class Solution:
                 if matrix[i][0]==0 or matrix[0][j]==0:
                     matrix[i][j] = 0
 
-        if isFirstRowZero:
+        if matrix[0][0]==0:
             for i in range(1, m):
                 matrix[0][i] = 0
 
