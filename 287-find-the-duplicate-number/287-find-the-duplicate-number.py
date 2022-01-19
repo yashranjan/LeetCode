3afca26cec633f36456f17131910543d05ec1214
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-01-19 20:28:56 +0530

    Time: 2548 ms (5.01%), Space: 29 MB (24.48%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,8 +1,16 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        numSet = set()
-        for i in nums:
-            if i in numSet:
-                return i
-            numSet.add(i)
-        return -1
+        n = len(nums)
+        ans = 0
+        for bit in range(32):
+            mask = 1<<bit
+            totCount = currCount = 0
+            for i in range(0, n):
+                if i&mask:
+                    totCount+=1
+                if nums[i]&mask:
+                    currCount+=1
+            if currCount > totCount:
+                print(mask)
+                ans|=mask
+        return ans
