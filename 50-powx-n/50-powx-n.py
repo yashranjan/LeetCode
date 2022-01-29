d76fe473574a2965b16dc88749d3c9d9a964ad1b
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-01-29 17:41:52 +0530

    Time: 47 ms (28.51%), Space: 13.9 MB (98.85%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -2,8 +2,6 @@ class Solution:
     def multiply(self, x, n):
         if n==1:
             return x
-        if n==0:
-            return 1
         ans = self.multiply(x, n//2)
         ans *= ans
         if n%2==1:
@@ -11,6 +9,8 @@ class Solution:
         return ans
     
     def myPow(self, x: float, n: int) -> float:
+        if n==0:
+            return 1
         toBeNegated = -1 if x<0 and n%2==1 else 1
         toBeDivided = 1 if n<0 else 0
         x, n = abs(x), abs(n)
