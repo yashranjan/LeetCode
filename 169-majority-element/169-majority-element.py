968ac89ce10081fc477a680258763f22489802d2
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-01-29 19:20:33 +0530

    Time: 471 ms (5.01%), Space: 15.5 MB (54.66%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -2,7 +2,6 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         ans = 0
         n = len(nums)
-        print([i.bit_length() for i in nums])
         for i in range(32):
             cnt = len([1 for j in nums if j&(1<<i)>0])
             if cnt > (n-cnt):
