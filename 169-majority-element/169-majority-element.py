93fcfd62887553343ca2dce949b958569948a46e
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-01-29 18:57:21 +0530

    Time: 168 ms (75.54%), Space: 15.5 MB (54.66%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,14 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        candidate, candidateCnt = math.inf, 0
+        candidate, candidateCnt = None, 0
         for i in nums:
-            if candidate == math.inf:
-                candidate = i
-            if i == candidate:
-                candidateCnt += 1
-            else:
-                candidateCnt -= 1
             if candidateCnt == 0:
-                candidate = math.inf
+                candidate = i
+            candidateCnt += (1 if i==candidate else -1)
         return candidate
         
