7a5f6adc05678433f49a47d79c2233e56c4e940f
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-01-31 20:47:09 +0530

    Time: 32 ms (95.13%), Space: 14 MB (96.08%) - LeetHub

--- 249-group-shifted-strings/249-group-shifted-strings.py
+++ 249-group-shifted-strings/249-group-shifted-strings.py
@@ -1,33 +1,7 @@
 class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
-        ans = []
-        n = len(strings)
-        marked = [False]*n
-        for i in range(n):
-            if marked[i]: continue
-
-            orig = strings[i]
-            marked[i] = True
-            currAns = [orig]
-
-            for j in range(i+1, n):
-                curr = strings[j]
-                if not marked[j] and len(orig) == len(curr):
-            
-                    diff = math.inf
-                    isSame = True
-                    
-                    for f,s in zip(orig, curr):
-                        if diff == math.inf:
-                            diff = (ord(s)-ord(f)+26)%26
-                        else:
-                            if (ord(s)-ord(f)+26)%26 != diff:
-                                isSame = False
-                                break
-                    if isSame:
-                        marked[j] = True
-                        currAns.append(curr)
-            
-            ans.append(currAns)
-        
-        return ans
+        mp = defaultdict(list)
+        for s in strings:
+            key = tuple((ord(c)-ord(s[0]))%26 for c in s)
+            mp[key].append(s)
+        return mp.values()
