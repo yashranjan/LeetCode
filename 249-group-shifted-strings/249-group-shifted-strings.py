22bb49d8781b84bdfb4716a8485c56c9471ebfa6
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-01-31 20:42:39 +0530

    Time: 44 ms (48.28%), Space: 13.9 MB (99.40%) - LeetHub

--- 249-group-shifted-strings/249-group-shifted-strings.py
+++ 249-group-shifted-strings/249-group-shifted-strings.py
@@ -5,15 +5,18 @@ class Solution:
         marked = [False]*n
         for i in range(n):
             if marked[i]: continue
-                
+
             orig = strings[i]
             marked[i] = True
             currAns = [orig]
+
             for j in range(i+1, n):
                 curr = strings[j]
                 if not marked[j] and len(orig) == len(curr):
+            
                     diff = math.inf
                     isSame = True
+                    
                     for f,s in zip(orig, curr):
                         if diff == math.inf:
                             diff = (ord(s)-ord(f)+26)%26
@@ -24,5 +27,7 @@ class Solution:
                     if isSame:
                         marked[j] = True
                         currAns.append(curr)
+            
             ans.append(currAns)
+        
         return ans
