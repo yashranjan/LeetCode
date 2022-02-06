b13ef2df26556e42c4d53ee20dfed73202b890b0
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-02-06 20:56:51 +0530

    Time: 228 ms (11.99%), Space: 14.3 MB (71.00%) - LeetHub

---
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.py
@@ -0,0 +1,25 @@
+class Solution:
+    def earliestAcq(self, logs: List[List[int]], n: int) -> int:
+        def find(a):
+            if parents[a] == a:
+                return a
+            parents[a] = find(parents[a])
+            return parents[a]
+        
+        def union(a, b):
+            a = find(a)
+            b = find(b)
+            parents[b] = a
+        
+        parents = [i for i in range(n)]
+        logs.sort(key = lambda x: x[0])
+        ans = -1
+        for log in logs:
+            a, b= log[1], log[2]
+            union(a, b)
+            print(parents)
+            if len([i for i in range(n) if i==parents[i]]) == 1:
+                ans = log[0]
+                break
+
+        return ans
