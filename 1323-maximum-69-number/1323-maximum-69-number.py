c1494ff33a08cdf20a289cd7e9ce7323910f0dbd
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-11-07 20:14:46 +0530

    Time: 57 ms (46.75%), Space: 13.9 MB (54.69%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -1,11 +1,4 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
         num_str = str(num)
-        idx = -1
-        try:
-            idx = num_str.index('6')
-        except ValueError as ve:
-            idx = -1
-        if idx!=-1:
-            num_str = num_str[:idx] + '9' + num_str[idx+1:]
-        return int(num_str)
+        return int(num_str.replace('6', '9', 1))
