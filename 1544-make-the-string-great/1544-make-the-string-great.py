54b8e6e1cec231bdf19daff5d2fe32bf377db39c
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-11-08 19:44:10 +0530

    Time: 56 ms (74.42%), Space: 14 MB (15.06%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -2,12 +2,6 @@ class Solution:
     def makeGood(self, s: str) -> str:
         if len(s)<=1:
             return s
-        cnt_lwr = cnt_upr = [0]*26
-        for i in s:
-            if 'A'<=i<='Z':
-                cnt_upr[ord(i)-ord('A')]+=1
-            else:
-                cnt_lwr[ord(i)-ord('a')]+=1
         idx = 0
         n = len(s)
         while idx<n:
