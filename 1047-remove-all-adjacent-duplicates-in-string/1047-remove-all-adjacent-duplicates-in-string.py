6d921108c59f57ac38d6f66887cec70a6c5db9d1
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-11-10 22:24:16 +0530

    Time: 135 ms (75.20%), Space: 14.8 MB (86.70%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
@@ -1,31 +1,10 @@
+from string import ascii_lowercase
 class Solution:
     def removeDuplicates(self, s: str) -> str:
-        if len(s)<=1:
-            return s
-        str_lst = list(s)
-        curr_idx = 0
-        next_idx = 1
-        lst_len = len(str_lst)
-        left_pos = []
-        while next_idx<lst_len:
-            # print(curr_idx, next_idx)
-            if str_lst[curr_idx]=='':
-                curr_idx+=1
-                if curr_idx==next_idx:
-                    next_idx+=1
-                else:
-                    continue
-            if str_lst[curr_idx]==str_lst[next_idx]:
-                str_lst[curr_idx] = str_lst[next_idx] = ''
-                curr_idx = -1 if not left_pos else left_pos.pop()
-                next_idx += 1
-                if curr_idx == -1:
-                    curr_idx=next_idx
-                    next_idx=next_idx+1
+        str_stk = []
+        for ch in s:
+            if not str_stk or str_stk[-1] != ch:
+                str_stk.append(ch)
             else:
-                left_pos.append(curr_idx)
-                curr_idx, next_idx = curr_idx+1, next_idx+1
-            # print(str_lst)
-
-        return ''.join(str_lst)
-        
+                str_stk.pop()
+        return ''.join(str_stk)
