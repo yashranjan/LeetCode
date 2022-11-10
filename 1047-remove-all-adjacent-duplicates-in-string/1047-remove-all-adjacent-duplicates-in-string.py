2dfae88bc69765041b744c6af6200cdc8ed769b6
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-11-10 22:16:10 +0530

    Time: 273 ms (22.21%), Space: 17.1 MB (5.52%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
@@ -2,15 +2,30 @@ class Solution:
     def removeDuplicates(self, s: str) -> str:
         if len(s)<=1:
             return s
+        str_lst = list(s)
         curr_idx = 0
-        lst_idx = len(s)-1
-        while curr_idx<lst_idx:
-            if curr_idx>=0 and curr_idx+1<=lst_idx and s[curr_idx]==s[curr_idx+1]:
-                s = s[:curr_idx]+s[curr_idx+2:]
-                curr_idx-=1
-            else:
+        next_idx = 1
+        lst_len = len(str_lst)
+        left_pos = []
+        while next_idx<lst_len:
+            # print(curr_idx, next_idx)
+            if str_lst[curr_idx]=='':
                 curr_idx+=1
-            lst_idx = len(s)-1
-        
-        return s
+                if curr_idx==next_idx:
+                    next_idx+=1
+                else:
+                    continue
+            if str_lst[curr_idx]==str_lst[next_idx]:
+                str_lst[curr_idx] = str_lst[next_idx] = ''
+                curr_idx = -1 if not left_pos else left_pos.pop()
+                next_idx += 1
+                if curr_idx == -1:
+                    curr_idx=next_idx
+                    next_idx=next_idx+1
+            else:
+                left_pos.append(curr_idx)
+                curr_idx, next_idx = curr_idx+1, next_idx+1
+            # print(str_lst)
+
+        return ''.join(str_lst)
         
