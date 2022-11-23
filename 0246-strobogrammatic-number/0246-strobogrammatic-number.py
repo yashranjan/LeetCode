231e18e70838366f206592d695593a5e9ec10b89
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-11-23 20:52:06 +0530

    Time: 45 ms (72.58%), Space: 13.9 MB (14.75%) - LeetHub

---
+++ 0246-strobogrammatic-number/0246-strobogrammatic-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isStrobogrammatic(self, num: str) -> bool:
+        mirror = ['0', '1', '-1', '-1', '-1', '-1', '9', '-1', '8', '6']
+        
+        new_str = ''.join([mirror[ord(i)-ord('0')] for i in reversed(num)])
+        # print(new_str)
+        return new_str == num
