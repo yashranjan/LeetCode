ff18b0245420896254c20c6f2c0da35263ed9f88
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-11-23 20:54:09 +0530

    Time: 76 ms (5.76%), Space: 13.8 MB (65.90%) - LeetHub

--- 0246-strobogrammatic-number/0246-strobogrammatic-number.py
+++ 0246-strobogrammatic-number/0246-strobogrammatic-number.py
@@ -3,5 +3,5 @@ class Solution:
         mirror = ['0', '1', '-1', '-1', '-1', '-1', '9', '-1', '8', '6']
         
         new_str = ''.join([mirror[ord(i)-ord('0')] for i in reversed(num)])
-        # print(new_str)
+
         return new_str == num
