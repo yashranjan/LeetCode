d7f2895b7e09a4048f3e91d04bc081ce18f3e17c
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-11-23 20:41:02 +0530

    Time: 227 ms (43.58%), Space: 14 MB (36.06%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -2,36 +2,46 @@ class Solution:
     def isValid(self, board, x, y, n=9):
         
         fnd = set()
-        for i in board[x]:
-            if i!='.' and i in fnd:
-                return False
-            fnd.add(i)
-        fnd.clear()
-        for i in range(n):
-            if board[i][y]!='.' and board[i][y] in fnd:
-                return False
-            fnd.add(board[i][y])
         maxX = x//3 + (2*(x//3))
         maxY = y//3 + (2*(y//3))
-        fnd.clear()
-        print(x, y, maxX, maxX+3, maxY, maxY+3)
         for i in range(maxX, maxX+3):
             for j in range(maxY, maxY+3):
-                print(board[i][j], end=' ')
                 if board[i][j]!='.' and board[i][j] in fnd:
                     return False
                 fnd.add(board[i][j])
-        print()
+
         return True
             
+    def isValidRow(self, board, row, n=9):
+        fnd = set()
+        for i in range(n):
+            if board[row][i]!='.' and board[row][i] in fnd:
+                return False
+            fnd.add(board[row][i])
+        return True
+    
+    def isValidCol(self, board, col, n=9):
+        fnd = set()
+        for i in range(n):
+            if board[i][col]!='.' and board[i][col] in fnd:
+                return False
+            fnd.add(board[i][col])
+        return True
         
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         n = 9
         
+        for i in range(n):
+            if not self.isValidRow(board, i):
+                return False
         
         for i in range(n):
-            for j in range(n):
-                if board[i][j]!='.' and not self.isValid(board, i, j):
+            if not self.isValidCol(board, i):
+                return False
+        
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                if not self.isValid(board, i, j):
                     return False
         
         return True
