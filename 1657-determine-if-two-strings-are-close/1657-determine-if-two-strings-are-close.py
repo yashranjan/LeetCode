f83371a7d882f515b56af2d9948266fee3a57697
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-12-02 21:15:08 +0530

    Time: 1288 ms (5.20%), Space: 15 MB (94.00%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -3,18 +3,18 @@ from collections import defaultdict
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         ans = True
-        cnt_dict_a = defaultdict(int)
-        cnt_dict_b = defaultdict(int)
+        cnt_dict_a = [0]*26
+        cnt_dict_b = [0]*26
         
         for i in word1:
-            cnt_dict_a[i] += 1
+            cnt_dict_a[ord(i)-ord('a')] += 1
         for i in word2:
-            cnt_dict_b[i] += 1
+            cnt_dict_b[ord(i)-ord('a')] += 1
         
         keys_a = set(word1)
         keys_b = set(word2)
         
-        values_a = sorted(cnt_dict_a.values())
-        values_b = sorted(cnt_dict_b.values())
+        values_a = sorted(cnt_dict_a)
+        values_b = sorted(cnt_dict_b)
         
         return keys_a == keys_b and values_a == values_b
