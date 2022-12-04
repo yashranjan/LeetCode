caadb34b560b1e6325f378c008d4f2c10205829f
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-12-04 12:29:01 +0530

    Time: 1702 ms (73.30%), Space: 24.8 MB (80.12%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.py
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -1,32 +1,15 @@
 class Solution:
     def minimumAverageDifference(self, nums: List[int]) -> int:
-        prefix_sum = []
-        suffix_sum = []
+        tot_sum = sum(nums)
         curr_sum = 0
         n = len(nums)
-        for i in range(n):
-            if i==0:
-                curr_sum = nums[i]
-            else:
-                curr_sum += nums[i]
-            prefix_sum.append(curr_sum)
-        
-        curr_sum = 0
-        print(nums)
-        for i in range(n-1, -1, -1):
-            if i==n-1:
-                curr_sum = nums[i]
-            else:
-                curr_sum += nums[i]
-            suffix_sum.append(curr_sum)
-        
-        suffix_sum.reverse()
         
         ans = 10**10
         ans_idx = -1
         for i in range(n):
-            left_val = (prefix_sum[i]//(i+1))
-            right_val = (suffix_sum[i+1]//(n-i-1)) if i+1<n else 0
+            curr_sum += nums[i]
+            left_val = (curr_sum//(i+1))
+            right_val = ((tot_sum-curr_sum)//(n-i-1)) if i+1<n else 0
             curr_val = abs(left_val-right_val)
             if curr_val<ans:
                 ans = curr_val
