141d804bcb375364a290daa012fb392a4696771e
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-12-12 22:02:37 +0530

    Time: 62 ms (20.89%), Space: 14 MB (12.35%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,6 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        ans = [0, 1, 2] + [0]*(n-2)
+        if n<=2:
+            return n
+        first, second = 1, 2
         for i in range(3, n+1):
-            ans[i] = ans[i-1]+ans[i-2]
-        return ans[n]
+            first, second = second, first+second
+        return second
