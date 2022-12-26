85c84c84fda09744c0a3610bb9c5001bdfb93841
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-12-26 22:38:48 +0530

    Time: 494 ms (89.99%), Space: 15.1 MB (82.53%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        lastGoodIdx = len(nums)-1
+        for i in range(len(nums)-2, -1, -1):
+            if i+nums[i]>=lastGoodIdx:
+                lastGoodIdx = i
+        
+        return lastGoodIdx==0
