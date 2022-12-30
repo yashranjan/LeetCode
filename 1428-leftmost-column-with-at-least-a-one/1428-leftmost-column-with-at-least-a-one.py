0b8b303c7a91aac38715c2b910b13ceac8a80905
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2022-12-30 20:18:56 +0530

    Time: 104 ms (96.09%), Space: 14.2 MB (88.13%) - LeetHub

---
+++ 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.py
@@ -0,0 +1,32 @@
+# """
+# This is BinaryMatrix's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class BinaryMatrix(object):
+#    def get(self, row: int, col: int) -> int:
+#    def dimensions(self) -> list[]:
+
+class Solution:
+    def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
+        
+        def getOnePos(binaryMatrix, row, cols):
+            l, r = 0, cols-1
+            curr = float('inf')
+            while l<=r:
+                m = (l+r)//2
+                val = binaryMatrix.get(row, m)
+                if not val:
+                    l = m+1
+                else:
+                    curr = m
+                    r = m-1
+            return curr
+        
+        ans = float('inf')
+        
+        rows, cols = binaryMatrix.dimensions()
+        
+        for row in range(rows):
+            ans = min(ans, getOnePos(binaryMatrix, row, cols))
+        
+        return ans if not ans==float('inf') else -1
