b15208ef8ac3345e2c7d5bf1f3df90cb26f2f395
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2023-01-16 20:26:22 +0530

    Time: 86 ms (75.67%), Space: 17.3 MB (52.22%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.py
@@ -0,0 +1,13 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        position = bisect.bisect(intervals, newInterval)
+        intervals.insert(position, newInterval)
+
+        answer = []
+        for i in range(len(intervals)):
+            if not answer or intervals[i][0] > answer[-1][1]:
+                answer.append(intervals[i])
+            else:
+                answer[-1][1] = max(answer[-1][1], intervals[i][1])
+
+        return answer
