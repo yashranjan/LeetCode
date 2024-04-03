fb9d2539b1fc56a58a0a3830584475ecd3924fef
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2024-04-03 15:54:26 +0530

    Time: 4559 ms (38.02%), Space: 16.6 MB (71.59%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,23 +1,31 @@
 class Solution:
-    def solve(self, board, m, n, x, y, word, idx):
-        if idx==len(word):
+    def solve(self, board: List[List[str]], coords_set: Set[Tuple[int, int]], curr_i, curr_j, word, curr_idx) -> bool:
+        if curr_idx == len(word):
             return True
+        elif curr_idx > len(word):
+            return False
+        coords_set.add((curr_i, curr_j))
+        next_coords = [(-1, 0), (1, 0), (0, 1), (0, -1)]
         
-        curr = board[x][y]
-        board[x][y] = '-'
-        for d_x, d_y in [(0, 1), (-1, 0), (0, -1), (1, 0)]:
-            n_x, n_y = x+d_x, y+d_y
-            if n_x>=0 and n_x<m and n_y>=0 and n_y<n and word[idx]==board[n_x][n_y]:
-                if self.solve(board, m, n, n_x, n_y, word, idx+1):
+        for (next_x, next_y) in next_coords:
+            new_x = curr_i + next_x
+            new_y = curr_j + next_y
+            if new_x<len(board) and new_x>=0 and new_y<len(board[0]) and new_y>=0 and (new_x, new_y) not in coords_set and board[new_x][new_y] == word[curr_idx]:
+                ret = self.solve(board, coords_set, new_x, new_y, word, curr_idx+1)
+                if ret:
                     return True
-        board[x][y] = curr
+        coords_set.remove((curr_i, curr_j))
         return False
+        
         
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m, n = len(board), len(board[0])
-        for i in range(m):
-            for j in range(n):
-                if board[i][j]==word[0]:
-                    if self.solve(board, m, n, i, j, word, 1):
+        for i_idx in range(len(board)):
+            for j_idx in range(len(board[i_idx])):
+                curr_char = board[i_idx][j_idx]
+                if curr_char == word[0]:
+                    coords_set = set()
+                    ret = self.solve(board, coords_set, i_idx, j_idx, word, 1)
+                    if ret:
                         return True
         return False
+        
