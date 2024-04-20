b26a681d38feb69f05c46abd800c738f9ed6ad6a
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2024-04-20 21:42:13 +0530

    Time: 1001 ms (26.50%), Space: 29.8 MB (91.00%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
@@ -1,17 +1,5 @@
 class Solution:
-    diff_coords = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    def findMax(self, land, curr_x, curr_y, n, m):
-        land[curr_x][curr_y] = -1
-        max_x, max_y = curr_x, curr_y
-        for d_x, d_y in self.diff_coords:
-            n_x, n_y = curr_x+d_x, curr_y+d_y
-            if n_x>=0 and n_y>=0 and n_x<n and n_y<m and land[n_x][n_y] == 1:
-                poss_x, poss_y = self.findMax(land, n_x, n_y, n, m)
-                max_x = max(max_x, poss_x)
-                max_y = max(max_y, poss_y)
-        
-        return (max_x, max_y)
-        
+    diff_coords = [(1, 0), (-1, 0), (0, 1), (0, -1)]        
         
     def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
         ans = []
@@ -20,7 +8,17 @@ class Solution:
         for i in range(n):
             for j in range(m):
                 if land[i][j] == 1:
-                    coords = self.findMax(land, i, j, n, m)
-                    ans.append([i, j, coords[0], coords[1]])
-        
+                    stk = [(i, j)]
+                    max_x, max_y = i, j
+                    while stk:
+                        curr_x, curr_y = stk.pop()
+                        land[curr_x][curr_y] = -1
+                        max_x = max(max_x, curr_x)
+                        max_y = max(max_y, curr_y)
+                        for d_x, d_y in self.diff_coords:
+                            n_x, n_y = curr_x+d_x, curr_y+d_y
+                            if n_x>=0 and n_y>=0 and n_x<n and n_y<m and land[n_x][n_y] == 1:
+                                stk.append((n_x, n_y))
+                    
+                    ans.append([i, j, max_x, max_y])
         return ans
