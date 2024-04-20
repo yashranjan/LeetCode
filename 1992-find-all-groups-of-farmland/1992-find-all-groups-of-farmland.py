88e795e0d0898ca838f5457d4dc419b0241eb50e
Yash Ranjan <32572228+yashranjan@users.noreply.github.com>
2024-04-20 21:48:30 +0530

    Time: 756 ms (92.00%), Space: 30.5 MB (60.50%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
@@ -1,24 +1,18 @@
 class Solution:
-    diff_coords = [(1, 0), (-1, 0), (0, 1), (0, -1)]        
-        
     def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
-        ans = []
-        n, m = len(land), len(land[0])
+        rows, cols = len(land), len(land[0])
+        farmlands = []
         
-        for i in range(n):
-            for j in range(m):
-                if land[i][j] == 1:
-                    stk = [(i, j)]
-                    max_x, max_y = i, j
-                    while stk:
-                        curr_x, curr_y = stk.pop()
-                        land[curr_x][curr_y] = -1
-                        max_x = max(max_x, curr_x)
-                        max_y = max(max_y, curr_y)
-                        for d_x, d_y in self.diff_coords:
-                            n_x, n_y = curr_x+d_x, curr_y+d_y
-                            if n_x>=0 and n_y>=0 and n_x<n and n_y<m and land[n_x][n_y] == 1:
-                                stk.append((n_x, n_y))
+        for i in range(rows):
+            for j in range(cols):
+                if land[i][j] == 1 and (i == 0 or land[i - 1][j] == 0) and (j == 0 or land[i][j - 1] == 0):
+                    bottom_row = i
+                    right_col = j
+                    while bottom_row + 1 < rows and land[bottom_row + 1][j] == 1:
+                        bottom_row += 1
+                    while right_col + 1 < cols and land[i][right_col + 1] == 1:
+                        right_col += 1
+
+                    farmlands.append([i, j, bottom_row, right_col])
                     
-                    ans.append([i, j, max_x, max_y])
-        return ans
+        return farmlands
